7173f726d0d2428c8b6d7a7a3f2d8c29b231c751
Sanjana Balaji <112532466+sanjanab23@users.noreply.github.com>
2024-03-26 18:24:53 +0530

    NumericFunctions

---
+++ NumericFunctions.sql
@@ -0,0 +1,14 @@
+-- ABS: Returns the absolute value of a number.
+SELECT ABS(-10) AS AbsoluteValue;
+
+-- ROUND: Rounds a number to a specified number of decimal places.
+SELECT ROUND(3.14159, 2) AS RoundedNumber;
+
+-- CEIL / CEILING: Returns the smallest integer greater than or equal to a given number.
+SELECT CEIL(3.7) AS CeilNumber;
+
+-- FLOOR: Returns the largest integer less than or equal to a given number.
+SELECT FLOOR(3.7) AS FloorNumber;
+
+-- SQRT: Returns the square root of a number.
+SELECT SQRT(16) AS SquareRoot;
